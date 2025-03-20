556b5482b218debdbed190df7b3328a5400eb885
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-03-20 12:19:34 +0100

    feat: Config & SQ3

--- factory_pattern/factory_method.py
+++ factory_pattern/factory_method.py
@@ -1,5 +1,14 @@
 import json
 import xml.etree.ElementTree as etree
+import sqlite3
+
+
+class Config:
+    def __init__(self, json_path: str = 'data/movies.json', xml_path: str = 'data/person.xml',
+                 sq3_path: str = 'data/person.sq3') -> None:
+        self.json_path = json_path
+        self.xml_path = xml_path
+        self.sq3_path = sq3_path
 
 
 class JSONDataExtractor:
@@ -21,11 +30,26 @@ class XMLDataExtractor:
         return self.tree
 
 
+class SQ3DataExtractor:
+    def __init__(self, filepath) -> None:
+        conn = sqlite3.connect(filepath)
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT id, firstName, lastName FROM persons")
+        self.data = cursor.fetchall()
+
+    @property
+    def parsed_data(self):
+        return self.data
+
+
 def dataextraction_factory(filepath):
     if filepath.endswith('json'):
         extractor = JSONDataExtractor
     elif filepath.endswith('xml'):
         extractor = XMLDataExtractor
+    elif filepath.endswith('sq3'):
+        extractor = SQ3DataExtractor
     else:
         raise ValueError('Cannot extract data from {}'.format(filepath))
     return extractor(filepath)
@@ -41,10 +65,11 @@ def extract_data_from(filepath):
 
 
 def main() -> None:
-    sqlite_factory = extract_data_from('data/person.sq3')
-    print()
+    config = Config()
 
-    json_factory = extract_data_from('data/movies.json')
+    # json
+    print("JSON")
+    json_factory = extract_data_from(config.json_path)
     json_data = json_factory.parsed_data
     print(f'Found: {len(json_data)} movies')
     for movie in json_data:
@@ -60,7 +85,9 @@ def main() -> None:
             print(f"Genre: {genre}")
         print()
 
-    xml_factory = extract_data_from('data/person.xml')
+    # xml
+    print("XML")
+    xml_factory = extract_data_from(config.xml_path)
     xml_data = xml_factory.parsed_data
     liars = xml_data.findall(f".//person[lastName='Liar']")
     print(f'found: {len(liars)} persons')
@@ -72,6 +99,19 @@ def main() -> None:
         [print(f"phone number ({p.attrib['type']}):", p.text)
          for p in liar.find('phoneNumbers')]
         print()
+
+    # sq3
+    print("SQ3")
+    sqlite_factory = extract_data_from(config.sq3_path)
+    sqlite_data = sqlite_factory.parsed_data
+    print(f'found: {len(sqlite_data)} persons')
+    for person in sqlite_data:
+        person_id, firstname, lastname = person
+        print(f'first name: {firstname}')
+        print(f'last name: {lastname}')
+        print()
+
+    print()
 
 
 if __name__ == "__main__":
