f9e6b1d2d98a9a6881733b60b88c6ac2f96224d3
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-03-27 12:24:55 +0100

    feat: tablet

--- builder/computer.py
+++ builder/computer.py
@@ -4,15 +4,23 @@ class Computer:
         self.memory = None  # in gigabytes
         self.hdd = None  # in gigabytes
         self.gpu = None
+        self.touch_screen = None
 
     def __str__(self):
-        info = (f'Memory: {self.memory}GB', f'Hard Disk: {self.hdd}GB', f'Graphics Card: {self.gpu}')
+        info = (f'Serial number: {self.serial}', f'Touch screen: {self.touch_screen}', f'Memory: {self.memory}GB', f'Hard Disk: {self.hdd}GB', f'Graphics Card: {self.gpu}')
         return '\n'.join(info)
 
 
 class ComputerBuilder:
+    serial_counter = 0
+
     def __init__(self):
-        self.computer = Computer('AG23385193')
+        serial_number = ComputerBuilder.serial_counter
+        ComputerBuilder.serial_counter += 1
+        self.computer = Computer(serial_number)
+
+    def configure_touch_screen(self, is_touch_screen):
+        self.computer.touch_screen = is_touch_screen
 
     def configure_memory(self, amount):
         self.computer.memory = amount
@@ -28,11 +36,12 @@ class HardwareEngineer:
     def __init__(self):
         self.builder = None
 
-    def construct_computer(self, memory, hdd, gpu):
+    def construct_computer(self, memory: int, hdd: int, gpu: str, touch_screen: bool):
         self.builder = ComputerBuilder()
         self.builder.configure_memory(memory)
         self.builder.configure_hdd(hdd)
         self.builder.configure_gpu(gpu)
+        self.builder.configure_touch_screen(touch_screen)
 
     @property
     def computer(self):
@@ -41,9 +50,13 @@ class HardwareEngineer:
 
 def main():
     engineer = HardwareEngineer()
-    engineer.construct_computer(hdd=500, memory=8, gpu='GeForce GTX 650 Ti')
+    engineer.construct_computer(hdd=500, memory=8, gpu='GeForce GTX 650 Ti', touch_screen=False)
     computer = engineer.computer
-    print(computer)
+    print(f"Computer:\n{computer}\n")
+
+    engineer.construct_computer(hdd=64, memory=2, gpu='Snapdragon', touch_screen=True)
+    tablet = engineer.computer
+    print(f"Tablet:\n{tablet}\n")
 
 
 if __name__ == '__main__':
