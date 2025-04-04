d6395fd8cf3127598300801f1f25f305a06f3fa7
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-04-04 23:54:48 +0200

    chore: memorization

--- decorator/number_sum.py
+++ decorator/number_sum.py
@@ -6,12 +6,29 @@ def number_sum(n):
     else:
         return n + number_sum(n - 1)
 
+sum_cache = {0: 0}
+
+
+def number_sum_cache(n):
+    """Returns the sum of the first n numbers"""
+    assert (n >= 0), 'n must be >= 0'
+    if n in sum_cache:
+        return sum_cache[n]
+    res = n + number_sum(n - 1)
+    # Add the value to the cache
+    sum_cache[n] = res
+    return res
+
 
 def main() -> None:
     from timeit import Timer
+    number = 100
 
-    t = Timer('number_sum(30)', 'from __main__ import number_sum')
-    print('Time: ', t.timeit())
+    functions = ['number_sum', 'number_sum_cache']
+
+    for f in functions:
+        t = Timer(f'{f}({number})', f'from __main__ import {f}')
+        print(f'Time of {f}: ', t.timeit())
 
 
 if __name__ == '__main__':
