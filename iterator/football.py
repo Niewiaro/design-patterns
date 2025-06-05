f4a499f484a4a15201366ca2b928a89c78876da4
Jakub <56446320+Niewiaro@users.noreply.github.com>
2025-06-05 12:24:14 +0200

    [feat] iterator

---
+++ iterator/football.py
@@ -0,0 +1,36 @@
+class FootballTeamIterator:
+    def __init__(self, members):
+        # the list of players and coaches
+        self.members = members
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.members):
+            val = self.members[self.index]
+            self.index += 1
+            return val
+        else:
+            raise StopIteration()
+
+
+class FootballTeam:
+    def __init__(self, members):
+        self.members = members
+
+    def __iter__(self):
+        return FootballTeamIterator(self.members)
+
+
+def main():
+    members = [f'player{str(x)}' for x in range(1, 23)]
+    members = members + ['coach1', 'coach2', 'coach3']
+    team = FootballTeam(members)
+    for member in team:
+        print(member)
+
+
+if __name__ == '__main__':
+    main()
